cda630195df591fca5a99cc88cf851f897b9abf9
Isha <ishadijcks@gmail.com>
2021-10-06 00:37:59 +0200

    2.2: Add IgtWallet to MyFeatures

--- src/my-game/MyFeatures.ts
+++ src/my-game/MyFeatures.ts
@@ -1,5 +1,5 @@
-import {IgtFeatures} from "incremental-game-template";
+import {IgtFeatures, IgtWallet} from "incremental-game-template";
 
 export interface MyFeatures extends IgtFeatures {
-
+    wallet: IgtWallet;
 }
